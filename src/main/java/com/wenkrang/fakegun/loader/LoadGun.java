993ceb1ae6ee0883a46db8a96082e26a37204fe3
Wenkrangha <104246750+Wenkrangha@users.noreply.github.com>
2025-05-31 19:06:40 +0800

    Update LoadGun.java

--- src/main/java/com/wenkrang/fakegun/loader/LoadGun.java
+++ src/main/java/com/wenkrang/fakegun/loader/LoadGun.java
@@ -166,7 +166,7 @@ public class LoadGun {
             CrossbowMeta crossbowMeta = (CrossbowMeta) itemMeta;
 
 // 设置弩的属性
-            crossbowMeta.addChargedProjectile(new ItemStack(Material.ARROW));
+            crossbowMeta.addChargedProjectile(new ItemStack(Material.FIREWORK_ROCKET));
 
 // 应用元数据
             itemStack.setItemMeta(crossbowMeta);
