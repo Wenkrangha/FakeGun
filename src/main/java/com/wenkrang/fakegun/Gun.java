531a005bdd74c07bf1b074b128dc5636db897c13
Fangtang3 <158278525+Fangtang3@users.noreply.github.com>
2026-02-05 17:54:07 +0800

    Revert "添加配置文件"
    This reverts commit 914d7e2136db2087d530274e8680fbcd2c3bd14d.

--- src/main/java/com/wenkrang/fakegun/Gun.java
+++ src/main/java/com/wenkrang/fakegun/Gun.java
@@ -2,7 +2,6 @@ package com.wenkrang.fakegun;
 
 import org.bukkit.inventory.ItemStack;
 
-@lombok.Data
 public class Gun {
     public String name;
     public int ticks;
@@ -14,6 +13,86 @@ public class Gun {
     public int damage;
     public int guneed;
     public ItemStack repice;
+
+    public ItemStack getRepice() {
+        return repice;
+    }
+
+    public void setRepice(ItemStack repice) {
+        this.repice = repice;
+    }
+
+    public int getGuneed() {
+        return guneed;
+    }
+
+    public void setGuneed(int guneed) {
+        this.guneed = guneed;
+    }
+
+    public void setAtBack(float atBack) {
+        AtBack = atBack;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setKeeps(int keeps) {
+        Keeps = keeps;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTicks(int ticks) {
+        this.ticks = ticks;
+    }
+
+    public void setReloadtime(int reloadtime) {
+        this.reloadtime = reloadtime;
+    }
+
+    public void setItemStack(ItemStack itemStack) {
+        this.itemStack = itemStack;
+    }
+
+    public ItemStack getItemStack() {
+        return itemStack;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public float getAtBack() {
+        return AtBack;
+    }
+
+    public int getKeeps() {
+        return Keeps;
+    }
+
+    public int getReloadtime() {
+        return reloadtime;
+    }
+
+    public int getTicks() {
+        return ticks;
+    }
 
     public void load() {
         FakeGun.Guns.add(this);
--- src/main/java/com/wenkrang/fakegun/Shoot.java
+++ src/main/java/com/wenkrang/fakegun/Shoot.java
@@ -1,6 +1,5 @@
 package com.wenkrang.fakegun;
 
-import com.wenkrang.fakegun.config.Config;
 import org.bukkit.*;
 import org.bukkit.entity.*;
 import org.bukkit.inventory.ItemStack;
@@ -9,24 +8,10 @@ import org.bukkit.util.Vector;
 
 import java.util.List;
 import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
 
 import static com.wenkrang.fakegun.event.FireE.applyRecoil;
 
 public class Shoot {
-
-    private static final float SPEED =
-            Float.parseFloat(Config.INSTANCE.get("bullet-speed").toString());
-
-    private static final float DAMAGE_ADDITION =
-            Float.parseFloat(Config.INSTANCE.get("bullet-damage-addition").toString());
-
-    private static final float BASE_RECOIL =
-            Float.parseFloat(Config.INSTANCE.get("base-recoil").toString());
-
-    private static final float RANDOMNESS =
-            Float.parseFloat(Config.INSTANCE.get("recoil-randomness").toString());
-
     public static void run (Player player, Gun gun) {
         Item item = player.getWorld().spawn(player.getEyeLocation(), Item.class);
 
@@ -36,39 +21,39 @@ public class Shoot {
 
         item.setPickupDelay(1145141919);
 
-        // 设置箭的速度（力度）
-        item.setVelocity(player.getLocation().getDirection().multiply(SPEED));
+        // 设置箭的速度（力度），这里假设是常规速度的五倍
+        item.setVelocity(player.getLocation().getDirection().multiply(5));
         new BukkitRunnable() {
 
             @Override
             public void run() {
-                Vector velocity = item.getVelocity();
+                if (true) {
+                    Vector velocity = item.getVelocity();
 
-                // 检查速度是否为零
-                if (velocity.equals(new Vector(0, 0, 0))) {
-                    // 物品正在移动
-                    item.remove();
-                    cancel();
-                }
-                item.setVelocity(player.getLocation().getDirection().multiply(5));
-                Location location = item.getLocation();
-                location.add(0, 1, 0);
-                location.getWorld().spawnParticle(Particle.SMOKE_NORMAL, location, 3, 0.1, 0.1, 0.1, 0);
-                List<Entity> nearbyEntities = item.getNearbyEntities(1.2, 1.2, 1.2);
-                for (Entity entity : nearbyEntities) {
-                    if (entity instanceof Damageable && !entity.equals(player)) {
-                        Damageable damageable = (Damageable) entity;
-                        damageable.damage(ThreadLocalRandom.current()
-                                .nextFloat(DAMAGE_ADDITION) + gun.getDamage(), player);
-                        applyRecoil((LivingEntity) damageable, player.getLocation().getDirection().multiply(-2)
-                        );
-                        Location location1 = damageable.getLocation();
-                        location1.setY(location1.getBlockY() + 1);
-                        damageable.getWorld().spawnParticle(Particle.BLOCK_CRACK, location1, gun.getDamage() * 12, Bukkit.createBlockData(Material.REDSTONE_BLOCK));
-                        damageable.getWorld().playSound(damageable.getLocation(), Sound.ENTITY_ARROW_HIT, 1.0F, 1.0F);
-
+                    // 检查速度是否为零
+                    if (velocity.equals(new Vector(0, 0, 0))) {
+                        // 物品正在移动
                         item.remove();
                         cancel();
+                    }
+                    item.setVelocity(player.getLocation().getDirection().multiply(5));
+                    Location location = item.getLocation();
+                    location.add(0, 1, 0);
+                    location.getWorld().spawnParticle(Particle.SMOKE_NORMAL, location, 3, 0.1, 0.1, 0.1, 0);
+                    List<Entity> nearbyEntities = item.getNearbyEntities(1.2, 1.2, 1.2);
+                    for (Entity entity : nearbyEntities) {
+                        if (entity instanceof Damageable && !entity.equals(player)) {
+                            Damageable damageable = (Damageable) entity;
+                            damageable.damage(new Random().nextInt(2) + gun.getDamage(), player);
+                            applyRecoil((LivingEntity) damageable, player.getLocation().getDirection().multiply(-2), 0.3, 0.1);
+                            Location location1 = damageable.getLocation();
+                            location1.setY(location1.getBlockY() + 1);
+                            damageable.getWorld().spawnParticle(Particle.BLOCK_CRACK, location1, gun.getDamage() * 12, Bukkit.createBlockData(Material.REDSTONE_BLOCK));
+                            damageable.getWorld().playSound(damageable.getLocation(), Sound.ENTITY_ARROW_HIT, 1.0F, 1.0F);
+
+                            item.remove();
+                            cancel();
+                        }
                     }
                 }
             }
